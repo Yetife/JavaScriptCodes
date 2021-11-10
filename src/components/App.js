5c33b7fe3558d167913f8514829f5da5c2d47152
Yetife <tifetreats21@gmail.com>
2021-11-10 11:57:26 +0100

    Introduction to reactjs

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import CountButton from "./CountButton/CountButton"
 import SearchBar from "./SearchBar/SearchBar"
 
 const App = () => {
-  // return(
-  //   <div>
-  //       <CountButton incrementBy={1} buttonColor={"blue"} />
-  //       <CountButton incrementBy={5} buttonColor={"red"}/>
-  //       <CountButton incrementBy={7} buttonColor={"green"}/>
-  //   </div>
-  // )
+  return(
+    <div>
+        <CountButton incrementBy={1} buttonColor={"blue"} />
+        <CountButton incrementBy={5} buttonColor={"red"}/>
+        <CountButton incrementBy={7} buttonColor={"green"}/>
+    </div>
+  )
     return <div>
         <SearchBar />
     </div>
