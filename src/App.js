fef9bb8fb0c7d39336ec7ab4105d14545fd0398e
Yetife <tifetreats21@gmail.com>
2021-11-12 17:30:07 +0100

    reactjs ewaApp file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Laptop from './views/Laptop'
+import './App.css'
 
 function App() {
   return(
-      <div>
-        <Laptop />
+      <div className="container">
+        <Laptop name={"Apple"} color={"gold"} madeIn={"China"} buttonColor={"magenta"}/>
+        <Laptop name={"Dell"} color={"pink"} madeIn={"Lagos"} buttonColor={"darkOrange"}/>
       </div>
   )
 }
