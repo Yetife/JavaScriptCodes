e6c99e0112676a573f6b4a4bb05e711aad257cfa
Yetife <tifetreats21@gmail.com>
2021-11-14 21:50:18 +0100

    reactjs ewaApp file

---
+++ src/components/Register/RegisterButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RegisterButton = () => {
+    return (
+        <div>
+            <a href="#">
+                <button className="register_btn">
+                    REGISTER
+                </button>
+            </a>
+        </div>
+    );
+};
+
+export default RegisterButton;
