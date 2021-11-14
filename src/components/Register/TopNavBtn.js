e045faf9685070bb66a3790d90149418ce92e7b8
Yetife <tifetreats21@gmail.com>
2021-11-14 21:50:44 +0100

    reactjs ewaApp file

---
+++ src/components/Register/TopNavBtn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TopNavBtn = () => {
+    return (
+        <div className="top_nav_btn">
+            <a href="Login.js" >
+                <div className="top_nav_btn">LOGIN</div>
+            </a>
+        </div>
+    );
+};
+
+export default TopNavBtn;
