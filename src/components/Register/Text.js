767d1b1c9b3db5aa302300445d5e61da71a88a3e
Yetife <tifetreats21@gmail.com>
2021-11-14 21:50:27 +0100

    reactjs ewaApp file

---
+++ src/components/Register/Text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Text = () => {
+    return (
+        <div className={"intro-text"}>
+            <h3>Welcome To Ewa</h3>
+            <p>Where your cravings for beans are satisfied</p>
+        </div>
+    );
+};
+
+export default Text;
