50d3b8b5d42a1e4b476a278f51b0f502791af652
Yetife <tifetreats21@gmail.com>
2021-11-21 23:49:44 +0100

    reactjs ewaApp file

--- src/components/Register/Input.js
+++ src/components/Register/Input.js
@@ -1,39 +1,18 @@
+
 import React from 'react';
 
-const Input = () => {
+const Input = (props) => {
+    let {label, fieldType, placeholder, name, value, onChange} = props
     return (
-        <div className={"input-container"}>
+        <div className={"input_container"} >
             <label>
-                Email
+                {label}
             </label>
-            <input type="tel"
-                   name="email"
-                   placeholder="Enter Email"
-                   className="register_input"/>
-                <label>
-                    First Name
-                </label>
-                <input type="text"
-                       name="first_name"
-                       placeholder="Enter first name"
-                       className="register_input"/>
-                    <label>
-                        Last Name
-                    </label>
-                    <input type="text"
-                           name="last_name"
-                           placeholder="Enter your last name"
-                           className="register_input"/>
-                        <label>
-                            Password
-                        </label>
-                        <input type="password"
-                               name="password"
-                               placeholder="Enter your password"
-                               className="register_input"/>
-                        {/*</label>*/}
+            <input type={fieldType} className="register_input"
+                   placeholder={placeholder} name={name} value={value} onChange={onChange} />
         </div>
     );
 };
 
 export default Input;
+
