b6a6505e7ace0a2be8d644a0da1008f29c887e51
Yetife <tifetreats21@gmail.com>
2021-11-21 23:49:21 +0100

    reactjs ewaApp file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import Laptop from './views/Laptop'
 import Register from './views/Register'
-import Login from './views/Login'
-import Home from './views/Home'
-import Auth from './ewa/views/auth/Auth'
+// import Login from './views/Login'
+// import Home from './views/Home'
+// import Auth from './ewa/views/auth/Auth'
 import './App.css'
 
 function App() {
@@ -10,7 +10,8 @@ function App() {
       <div className="container">
         {/*<Laptop name={"Apple"} color={"gold"} madeIn={"China"} buttonColor={"magenta"}/>*/}
         {/*<Laptop name={"Dell"} color={"pink"} madeIn={"Lagos"} buttonColor={"darkOrange"}/>*/}
-          <Auth/>
+        {/*  <Auth/>*/}
+          <Register />
       </div>
   )
 }
