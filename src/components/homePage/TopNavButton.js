f16a0da7acd7f0241d99bcae4e04459aad3a6af1
Yetife <tifetreats21@gmail.com>
2021-11-21 23:50:38 +0100

    reactjs ewaApp file

--- src/components/homePage/TopNavButton.js
+++ src/components/homePage/TopNavButton.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const TopNavButton = () => {
     return (
-        <div>
-            <a href="#" >
-                <div className="top_nav_btn">HOME</div>
-            </a>
-        </div>
+
+                <button className="top_nav_btn">
+                    HOME
+
+                </button>
     );
 };
 
