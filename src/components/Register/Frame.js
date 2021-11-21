8dd06376741873b260ffe57d941b16e83377cdcd
Yetife <tifetreats21@gmail.com>
2021-11-21 23:49:31 +0100

    reactjs ewaApp file

--- src/components/Register/Frame.js
+++ src/components/Register/Frame.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Text from './Text';
-import Input from './Input'
+// import Input from './Input'
 import backgroundCircle from '../../assets/circle2.svg'
 import RegisterButton from './RegisterButton'
 
@@ -8,8 +8,8 @@ const Frame = () => {
     return (
         <div className={"frame-container"}>
             <Text />
-            <Input />
-            <img src={backgroundCircle} className={"bottom-circle"} />
+            {/*<Input />*/}
+            <img src={backgroundCircle} className={"bottom-circle"} alt={"circle"}/>
             <RegisterButton />
         </div>
     );
